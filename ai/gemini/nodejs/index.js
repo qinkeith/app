cde6bddf6680c186e21ceda1f5ef2cdb11f14ac1
qinkeith <69738474+qinkeith@users.noreply.github.com>
2025-02-16 15:09:19 -0500

    removed backend UI

--- ai/gemini/nodejs/index.js
+++ ai/gemini/nodejs/index.js
@@ -1,10 +1,13 @@
 // In index.js
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const generateContent = require("./routes/gemini.js");
 
+// Enable CORS
+app.use(cors());
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -13,10 +16,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
 app.post("/gemini", generateContent);
 
 app.listen(3000, () => {
